23e7224b7306ab765fb47105e74996d600bcb83a
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-11-20 12:14:56 -0300

    feat: import middlewares

--- backend/src/routes/main.ts
+++ backend/src/routes/main.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express'
-
-import * as authController from '../controllers/auth'
-import * as userController from '../controllers/user'
-import { verifyJWT } from '../utils/jwt'
+import * as authController from '../controllers/auth-controller'
+import * as userController from '../controllers/user-controller'
+import { verifyJWT } from '../middlewares/auth.middleware'
 
 export const mainRouter = Router()
 
 mainRouter.post('/auth/signup', authController.signup)
 mainRouter.post('/auth/signin', authController.signin)
 mainRouter.get('/user/:slug', verifyJWT, userController.getUser)
-
-
