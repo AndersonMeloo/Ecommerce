3583aa9028fdd854e057134c3ddd7d647f0ec932
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-11-22 17:28:13 -0300

    refactor: remove slug and add id

--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -4,11 +4,11 @@ import { signinSchema } from "../schemas/signin";
 import { createUser, findUserByEmail } from "../services/user";
 import { hash, compare } from "bcrypt-ts";
 import { createJWT } from "../utils/jwt";
-import slug from "slug";
 
 export const signup: RequestHandler = async (req, res) => {
 
-    const safeData = signupSchema.safeParse(req.body) || {};
+    const safeData = signupSchema.safeParse(req.body);
+
     if (!safeData.success) {
         return res.json({ error: safeData.error.flatten().fieldErrors });
     }
@@ -20,17 +20,13 @@ export const signup: RequestHandler = async (req, res) => {
 
     const hashedPassword = await hash(safeData.data.password, 10);
 
-    const userSlug = slug(safeData.data.name + Date.now());
-
     const newUser = await createUser({
-
-        slug: userSlug,       
         name: safeData.data.name,
         email: safeData.data.email,
         password: hashedPassword
     });
 
-    const token = createJWT(newUser.email);
+    const token = createJWT(newUser.id);
 
     res.status(201).json({
         token,
@@ -52,11 +48,14 @@ export const signin: RequestHandler = async (req, res) => {
     const user = await findUserByEmail(safeData.data.email);
     if (!user) return res.status(401).json({ error: 'Acesso negado' });
 
+    if (!user.password) {
+        return res.status(500).json({ error: "Erro: Usuário não possui Senha." });
+    }
+
     const verifyPass = await compare(safeData.data.password, user.password);
-    
     if (!verifyPass) return res.status(401).json({ error: 'Acesso negado' });
 
-    const token = createJWT(user.email);
+    const token = createJWT(user.id);
 
     res.json({
         token,
