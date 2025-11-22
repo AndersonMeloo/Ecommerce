1680af6e9f9cdb9c13e31f80e6ba38ee0f54837c
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-11-22 15:43:29 -0300

    feat: create function JWT generate tokens

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,5 +1,11 @@
 import jwt from "jsonwebtoken"
+import { appConfig } from "../config/app"
 
 export const createJWT = (slug: string) => {
-    return jwt.sign({ slug }, process.env.JWT_SECRET as string)
+
+    return jwt.sign(
+
+        { slug },
+        appConfig.jwtSecret as string
+    )
 }
