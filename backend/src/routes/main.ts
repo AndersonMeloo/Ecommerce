b1f294e3426f0b806e6b23991a73e1c1fabf4243
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-11-23 02:12:04 -0300

    feat create router notifications

--- backend/src/routes/main.ts
+++ backend/src/routes/main.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import * as authController from '../controllers/auth-controller'
 import * as userController from '../controllers/user-controller'
+import * as notificationsController from '../controllers/notifications-controller';
 import { verifyJWT } from '../middlewares/auth.middleware'
 
 export const mainRouter = Router()
@@ -8,3 +9,4 @@ export const mainRouter = Router()
 mainRouter.post('/auth/signup', authController.signup)
 mainRouter.post('/auth/signin', authController.signin)
 mainRouter.get('/user/:id', verifyJWT, userController.getUser)
+mainRouter.get('/user/notifications', verifyJWT, notificationsController.getNotifications);
