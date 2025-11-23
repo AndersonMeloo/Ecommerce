97947f21dda58d011e228d88d4ec0efbf50429cd
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-11-23 02:13:45 -0300

    feat: new text

--- backend/src/controllers/notifications-controller.ts
+++ backend/src/controllers/notifications-controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
 
 const notifications = [
-    { id: 1, message: 'Pagamentos Pendentes' }
+    { id: 1, message: 'Parcelas Pendentes' }
 ]
 
 export const getNotifications = (req: Request, res: Response) => {
