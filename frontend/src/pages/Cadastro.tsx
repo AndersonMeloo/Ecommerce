ac62d94a3ddf3fc5416f156f2601ebd0462a766e
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-11-22 23:46:58 -0300

    chore: import modal

--- frontend/src/pages/Cadastro.tsx
+++ frontend/src/pages/Cadastro.tsx
@@ -1,20 +1,21 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import styles from '../styles/App.module.scss';
+import styles from '../styles/Login-Register.module.scss';
 import { authAPI } from "../services/auth";
+import Modal from "../components/Modals";
 
 export default function Cadastro() {
 
   const navigate = useNavigate();
-
   const [form, setForm] = useState({ name: "", email: "", password: "" });
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
 
   async function handleCadastro() {
 
     try {
-
+      
       setLoading(true);
       setError(null);
 
@@ -25,28 +26,45 @@ export default function Cadastro() {
       });
 
       console.log('Cadastro realizado!!:', data);
-      alert('Cadastro realizado com sucesso! Faça o login para continuar.');
-      navigate('/login');
+      setShowSuccess(true)
 
     } catch (err: any) {
-
+      
       if (err.message.includes('password')) {
         setError(`Erro em senha: ${err.message}`);
-
       } else {
         setError(err.message || 'Erro ao cadastrar');
       }
-
     } finally {
       setLoading(false);
     }
   }
 
+  const handleModalClose = () => {
+
+    setShowSuccess(false);
+    navigate('/login'); 
+  };
+
   return (
 
-    <div className={styles.loginContainer}>
+    <div className={styles.containerLogin}>
+      
+      <Modal
+        estaAberto={showSuccess}
+        fechar={handleModalClose}
+        titulo="Cadastro Realizado!"
+        mensagem="Cadastro realizado com sucesso! Faça o login para continuar."
+        tipo="sucesso"
+      />
 
-      <h1 className={styles.loginTitle}>Cadastro</h1>
+      <div className={styles.containerCircle}>
+        <div className={styles.circles}></div>
+        <div className={styles.circles}></div>
+        <div className={styles.circles}></div>
+      </div>
+
+      <h1 className={styles.login}>Cadastro</h1>
 
       <input
         type="text"
@@ -81,7 +99,7 @@ export default function Cadastro() {
       <p>
         Já tem conta? <Link to="/login">Faça login</Link>
       </p>
-      
+
     </div>
   );
 }
